e7386aceff96a6fc8c132783b4cc1b61a43cafff
Alokxt <manawatalok@gmail.com>
2025-09-02 22:54:29 +0530

    ORM models with student api

--- minitasks/tasks/views.py
+++ minitasks/tasks/views.py
@@ -1,21 +1,42 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
-student_list =  ['Ram','Aam','Sham','Kam','Nam']
+from .models import *
+student_list2 =  ['Ram','Aam','Sham','Kam','Nam']
 # Create your views here.
 @api_view(['GET'])
 def hello_world(request):
     return JsonResponse({"Message":"Hello World"})
 
+
+
+def student_list():
+    l =[]
+    stds = Student.objects.all()
+    for s in stds:
+        l.append(s.name)
+    return l 
+
 @api_view(['GET','POST','PUT','DELETE'])
 def students(request):
     if request.method == "GET":
-        return JsonResponse({"Succes":True,"Stuents":student_list})
+        l = student_list()
+        return JsonResponse({"Succes":True,"Stuents":l})
     if request.method == "POST":
         get_student = request.data 
         list_std = get_student["Student_list"]
-        student_list.extend(list_std)
-        return JsonResponse({"Succes":True,"Updated Stuents":student_list})
+        for d in list_std:
+            s = Student()
+            s.name = d["name"] if "name" in d else None 
+            s.age = d["age"] if "age" in d else None 
+            s.class_name = d["class"] if "class" in d else None 
+            s.save()
+
+        
+        l = student_list()
+
+        return JsonResponse({"Succes":True,"Updated Stuents":l})
+    
     
 
 
