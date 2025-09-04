bf54aedc545d840fe1fd9d5701f34c3a26fa0869
Aashay205 <jainaashay4@gmail.com>
2025-09-04 08:27:16 +0000

    added e-commerce store

---
+++ shopping-cart-react/src/products.js
@@ -0,0 +1,19 @@
+import img1 from "./assets/products/1.png";
+import img2 from "./assets/products/2.png";
+import img3 from "./assets/products/3.png";
+import img4 from "./assets/products/4.png";
+import img5 from "./assets/products/5.png";
+import img6 from "./assets/products/6.webp";
+import img7 from "./assets/products/7.webp";
+import img8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  { id: 1, productName: "Product 1", price: 10.99, productImage: img1 },
+  { id: 2, productName: "Product 2", price: 12.99, productImage: img2 },
+  { id: 3, productName: "Product 3", price: 8.99, productImage: img3 },
+  { id: 4, productName: "Product 4", price: 15.99, productImage: img4 },
+  { id: 5, productName: "Product 5", price: 9.99, productImage: img5 },
+  { id: 6, productName: "Product 6", price: 11.99, productImage: img6 },
+  { id: 7, productName: "Product 7", price: 14.99, productImage: img7 },
+  { id: 8, productName: "Product 8", price: 13.99, productImage: img8 },
+];
